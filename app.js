4dbb18f3f4908fddb8f9af2e8d8c6efddfeb9aa5
Aun-Ali-12 <aunali29331@gmail.com>
2025-07-20 01:50:42 +0500

    login/Signup added

--- app.js
+++ app.js
@@ -6,6 +6,125 @@ let selectDifficulty = document.getElementById("add-option");
 let cuisine = [];
 let selectCuisine = document.getElementById("add-cuisine");
 let searchBar = document.getElementById("searchBar");
+let loginBtn = document.getElementById("button-login");
+let signupForm = document.getElementById("signup-form");
+let signinBtn = document.getElementById("signin");
+let loginForm = document.getElementById("login-form");
+let signupbtn = document.getElementById("signup");
+let suBtn = document.getElementById("btn-su");
+let liBtn = document.getElementById("btn-li"); //btn to loginto an account in login form
+let cancelBtn = document.getElementById("btn-cancel"); //btn within login form to navigate user to the home page
+let userSuDetails = JSON.parse(localStorage.getItem("userSignup")) || [];
+
+//Onclick Login btn
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    window.location.href = "login.html";
+  });
+}
+
+//login.html will look like after click on login btn
+if (loginForm) {
+  signupForm.style.display = "none";
+}
+
+//when on login html, login form will be appear on that form there is a signup btn this function will be perform on the click of it.
+if (signupbtn) {
+  signupbtn.addEventListener("click", () => {
+    loginForm.style.display = "none";
+    signupForm.style.display = "block";
+  });
+}
+
+//Account creation logic
+//Creating account or signing up functionality will perform here
+let suPass = document.getElementById("signup-pass");
+if (suPass) {
+  suPass.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+      let suEmail = document.getElementById("signup-email");
+      let suDetail = {
+        email: suEmail.value,
+        password: suPass.value,
+      };
+      let checkAlreadyDetail = userSuDetails.find(
+        (value) => suEmail.value == value.email
+      );
+      if (checkAlreadyDetail) {
+        console.log(userSuDetails);
+        Swal.fire({
+          icon: "error",
+          title: "Email already exists!",
+          text: "Try another email",
+          timer: 2000,
+          showConfirmButton: false,
+        });
+      } else {
+        console.log(userSuDetails);
+        userSuDetails.push(suDetail);
+        localStorage.setItem("userSignup", JSON.stringify(userSuDetails));
+        Swal.fire({
+          icon: "success",
+          title: "Account created successfully!",
+          text: "navigating to login page..",
+          timer: 2000,
+          showConfirmButton: false,
+        });
+        loginForm.style.display = "block"; //shows login form to log into account
+        signupForm.style.display = "none"; //hides signup form just as acc will get created
+      }
+    }
+  });
+}
+
+//Login logic
+let checkEmail = document.getElementById("signin-email");
+let checkPass = document.getElementById("signin-pass");
+if (checkPass) {
+  checkPass.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+      let checkBoth = userSuDetails.find(
+        (user) =>
+          user.email == checkEmail.value && user.password == checkPass.value
+      );
+      if (checkBoth) {
+        Swal.fire({
+          icon: "success",
+          title: "Login successfull!",
+          text: "navigating to home page..",
+          timer: 2000,
+          showConfirmButton: false,
+        });
+        setTimeout(() => {
+          window.location.href = "index.html";
+        }, 2000);
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Incorrect email or password!",
+          timer: 2000,
+          showConfirmButton: false,
+        });
+      }
+    }
+  });
+}
+
+//onclick signin btn which is within signup form
+if (signinBtn) {
+  signinBtn.addEventListener("click", () => {
+    signupForm.style.display = "none";
+    loginForm.style.display = "block";
+  });
+}
+
+//functionality on cancel button:
+if (cancelBtn) {
+  cancelBtn.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+}
+
 async function fetchApi() {
   //Fetching api
   let response = await fetch(url);
